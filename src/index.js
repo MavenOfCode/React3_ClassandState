002b5e8d22133db1ed77b797c468795c662924a0
Suzanne Richman <srichm@ext.uber.com>
2019-06-23 15:42:20 -0700

    refactoring but keeping notes and earlier code with comments

--- src/index.js
+++ src/index.js
@@ -1,56 +1,50 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import SeasonDisplay from './SeasonDisplay';
-import { thisExpression } from '@babel/types';
+
 
 class App extends React.Component {
   // First thing called the very first time this class is first created
-  // Way to initialize State
+  // Constructor function is one way to initialize State
   // good for one -time set
   // Best practice - do not do data loading in the constructor
   // OPTIONAL
-  constructor(props){
-    // Calls base/parent class React.Component constructor function
-    super(props);
+  // constructor(props){
+  //   // Calls base/parent class React.Component constructor function
+  //   super(props);
 
-    // Initialize a property of state with a key value pair
-    // ONLY TIME we directly assign this.state value
-    this.state = { lat: null , errorMessage: ''};
+  //   // Initialize a property of state with a key value pair
+  //   // ONLY TIME we directly assign this.state value
+  //   this.state = { lat: null , errorMessage: ''};
+  // }
 
-  }
+  // Alternate clean way to initialize state
+  state = {lat :null, errorMessage: ''};
 
   // OPTIONAL will be called one time at the start of the lifecycle
   // good for one-time setup
   // best practice to do data loading here rather than constructor
-  // componentDidMount()
-
-  // render is called before this method each time
-  // OPTIONAL will be called anytime the component updates itself
-  // good place to do addtional data loading for components
-  // componentDidUpdate()
-
-  // OPTIONAL  could be called if the component was no longer needed
-  // componentWillUnmount()
-
   componentDidMount(){
     window.navigator.geolocation.getCurrentPosition(
       // argument number 1 - aka succcess callback
-      (position)=> {
-        // called setState (a function that is available from extension of parent React.Component Class)
-        this.setState({lat: Math.floor(position.coords.latitude)})
-      },
+      // called setState (a function that is available from extension of parent React.Component Class)
+      position => this.setState({lat: Math.floor(position.coords.latitude)}),
       // argument number 2 - error callback
       // if user declines location request or location not available)
-      (err) => {
-        this.setState( {errorMessage: err.message})
-      }
+      err => this.setState( {errorMessage: err.message})
       );
       console.log("my component is loaded")
   }
 
+  // render is called before this method each time
+  // OPTIONAL will be called anytime the component updates itself
+  // good place to do addtional data loading for components
   componentDidUpdate(){
     console.log("my component did update")
   }
+
+    // OPTIONAL  could be called if the component was no longer needed
+  // componentWillUnmount()
 
   // React requires render method defined for any React.Component class extension
   // Can be called fequently so dont' put other methods/calls in it
