e8c6857010509ffb5cf1feafae4d60bbbe6277a2
Suzanne Richman <srichm@ext.uber.com>
2019-06-23 14:50:36 -0700

    working on state

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import SeasonDisplay from './SeasonDisplay';
+import { thisExpression } from '@babel/types';
 
 class App extends React.Component {
   // First thing called the very first time this class is first created
@@ -10,20 +11,36 @@ class App extends React.Component {
     super(props);
 
     // Initialize a property of state with a key value pair
-    this.state ={ lat: null };
+    // ONLY TIME we directly assign this.state value
+    this.state = { lat: null , errorMessage: ''};
+
+    window.navigator.geolocation.getCurrentPosition(
+      // argument number 1 - aka succcess callback
+      (position)=> {
+        // called setState (a function that is available from extension of parent React.Component Class)
+        this.setState({lat: Math.floor(position.coords.latitude)})
+      },
+      // argument number 2 - error callback
+      // if user declines location request or location not available)
+      (err) => {
+        this.setState( {errorMessage: err.message})
+      }
+      );
+
   }
 
   // React requires render method defined for any React.Component class extension
+  // Can be called fequently so dont' put other methods/calls in it
   render (){
-    window.navigator.geolocation.getCurrentPosition(
-      // argument number 1 - aka succcess callback
-      (position)=> console.log(position),
-      // argument number 2 - error callback
-      // if user declines location request or location not available)
-      (err) => console.log(err)
+    if(this.state.errorMessage && !this.state.lat){
+      return <div>Error: {this.state.errorMessage}</div>;
+    }
+    if(this.state.lat && !this.state.errorMessage){
+      return <div> Latitude: {this.state.lat} </div>
+    }
+    return (
+      <div> ...loading</div>
       );
-
-      return <div> Latitude: </div>;
   }
 }
 
