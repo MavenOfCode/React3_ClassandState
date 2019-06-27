cef104c6f8cfc5b5e8f9e0f5f54a464346789f94
Suzanne Richman <srichm@ext.uber.com>
2019-06-27 15:22:20 -0700

    making a little practice while product builds

--- src/SeasonDisplay.js
+++ src/SeasonDisplay.js
@@ -1,5 +1,16 @@
 import React from 'react';
 
+const seasonConfig = {
+  summer: {
+    text: 'Lets hit the beach',
+    icon: 'sun',
+  },
+  winter: {
+    text: 'Burr it is cold',
+    icon: 'snowflake',
+  },
+}
+
 const getSeason = (lat, month) => {
  if(month >2 && month < 9){
   return lat >0 ? 'summer' : 'winter';
@@ -10,10 +21,12 @@ const getSeason = (lat, month) => {
 
 const SeasonDisplay = props =>{
   const season = getSeason(props.lat,new Date().getMonth());
-  console.log(season)
+ const {text, iconName} = seasonConfig[season];
   return(
     <div>
-     Welcome to the {season} display!
+      <i className={`${iconName} icon`} size={30}/>
+     <h1>{text}</h1>
+     <i className={`${iconName} icon`} style={{alignContent:'center'}}size={30}/>
     </div>
   );
 }
